ebd369d92a55f45fe43fbed14feec77ce4e963bb
Parker Smith <parkerssmith0@gmail.com>
2023-03-14 16:25:02 -0600

    Mission11

---
+++ Mission09_parkersm/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_parkersm.Models;
+
+namespace Mission09_parkersm.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket b)
+        {
+            basket = b;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
